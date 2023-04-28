ed41b16820bb4d11827fcb2e3268b01277a4a174
Hanna Jeong <106164398+hanna3019@users.noreply.github.com>
2023-04-28 21:41:35 +0900

    Update 모음 제거.js
    추가로 공부한 내용 추가

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -2,3 +2,15 @@ function solution(my_string) {
     var answer = my_string.replace(/a|e|i|o|u/g,'');
     return answer;
 }
+
+/*
+정규식 사용
+/[aeiou]/g 로도 가능!
+-----------------------
+g	모든 패턴 체크(global)
+i	대소문자 구별 없이 체크
+m	여러줄 체크
+^	처음
+$	끝
+.	한문자
+*/
