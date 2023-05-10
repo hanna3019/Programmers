86fbbe550605f348a0c607c935a94f65a4fe1919
Hanna Jeong <106164398+hanna3019@users.noreply.github.com>
2023-05-10 21:11:35 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,6 @@
 function solution(hp) {
-    let answer = 0;
     let fir = Math.floor(hp/5);
     let sec = Math.floor((hp-fir*5)/3);
     let thd = Math.floor(hp-fir*5-sec*3);
-    return answer = fir + sec + thd;
+    return fir + sec + thd;
 }
