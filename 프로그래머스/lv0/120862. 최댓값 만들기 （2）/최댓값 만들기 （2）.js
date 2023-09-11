2366780d5da5a1ff3d6e60de48b23677512b8a8a
Hanna Jeong <106164398+hanna3019@users.noreply.github.com>
2023-09-11 13:25:31 +0900

    Update 최댓값 만들기 （2）.js

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    let sort = numbers.sort((a,b) => a-b);
-    return numbers[0]*numbers[1]>numbers[numbers.length-1]*numbers[numbers.length-2]?numbers[0]*numbers[1]:numbers[numbers.length-1]*numbers[numbers.length-2];
+    numbers.sort((a,b) => a-b);
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]);
 }
